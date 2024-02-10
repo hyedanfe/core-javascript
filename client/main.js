c0ee2e47f6e0e0895914edba0beb6f095dd47466
hyedanfe <haedan4637@gmail.com>
2024-02-11 00:22:04 +0900

    mission02

--- client/main.js
+++ client/main.js
@@ -1,5 +1,61 @@
-const a = 10;
+/* global getNode, insertLast, clearContents, getNodes, refError,
+  typeError */
 
-console.log();
+import {
+  getNode,
+  getNodes,
+  insertLast,
+  clearContents,
+  refError,
+  typeError,
+} from './lib/index.js';
 
-console.log("안녕 자바스크립트!");
+function phase1() {
+  const first = getNode('#firstNumber');
+  const second = getNode('#secondNumber');
+  const result = getNode('.result');
+  const clear = getNode('#clear');
+
+  function handleInput() {
+    const firstValue = Number(first.value);
+    const secondValue = Number(second.value);
+    const total = firstValue + secondValue;
+
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+    clearContents(first);
+    clearContents(second);
+    result.textContent = '-';
+  }
+
+  first.addEventListener('input', handleInput);
+  second.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
+}
+
+const calculator = getNode('.calculator');
+const result = getNode('.result');
+const clear = getNode('#clear');
+const numberInputs = Array.from(getNodes('input:not(#clear)'));
+
+function handleInput() {
+  const total = numberInputs.reduce((acc, cur) => acc + Number(cur.value), 0);
+
+  clearContents(result);
+  insertLast(result, total);
+}
+
+function handleClear(e) {
+  e.preventDefault();
+
+  numberInputs.forEach(clearContents);
+
+  result.textContent = '-';
+}
+
+calculator.addEventListener('input', handleInput);
+clear.addEventListener('click', handleClear);
